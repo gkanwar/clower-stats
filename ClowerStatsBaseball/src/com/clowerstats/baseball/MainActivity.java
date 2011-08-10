a2a0929cdf471c07e4b3405436a7f3d3fc3b32e6
GurtejKanwar <GurtejKanwar@8e3d5ed4-8447-37a5-6c51-38c1511ff847>
2011-08-10 19:04:35 +0000

    Most of the framework working.

--- ClowerStatsBaseball/src/com/clowerstats/baseball/MainActivity.java
+++ ClowerStatsBaseball/src/com/clowerstats/baseball/MainActivity.java
@@ -1,11 +1,14 @@
 package com.clowerstats.baseball;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends Activity
@@ -27,16 +30,36 @@ public class MainActivity extends Activity
         		this, R.array.teamArray, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         teamSpinner.setAdapter(adapter);
+        
+        positionSpinner.setOnItemSelectedListener(new OnPositionSelectedListener());
+        teamSpinner.setOnItemSelectedListener(new OnTeamSelectedListener());
+        
+        Button button = (Button) findViewById(R.id.searchButton);
+        button.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+		        EditText et = (EditText) findViewById(R.id.searchBar);
+	        	Intent intent = new Intent(MainActivity.this, MatrixActivity.class);
+	        	intent.putExtra("type", "player");
+	        	intent.putExtra("player", et.getText().toString());
+	        	startActivity(intent);
+			}
+		});
     }
     
     public class OnPositionSelectedListener implements OnItemSelectedListener
     {
         public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
         {
-        	
+        	Intent intent = new Intent(MainActivity.this, MatrixActivity.class);
+        	intent.putExtra("type", "position");
+        	intent.putExtra("position", parent.getItemAtPosition(pos).toString());
+        	startActivity(intent);
         }
 
-        public void onNothingSelected(AdapterView parent)
+        public void onNothingSelected(AdapterView<?> parent)
         {
           // Do nothing.
         }
@@ -45,10 +68,13 @@ public class MainActivity extends Activity
     {
         public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
         {
-        	
+        	Intent intent = new Intent(MainActivity.this, MatrixActivity.class);
+        	intent.putExtra("type", "team");
+        	intent.putExtra("team", parent.getItemAtPosition(pos).toString());
+        	startActivity(intent);
         }
 
-        public void onNothingSelected(AdapterView parent)
+        public void onNothingSelected(AdapterView<?> parent)
         {
           // Do nothing.
         }
